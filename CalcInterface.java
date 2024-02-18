a872ef32ee705ca002a993229110852a8fe41ef6
Waddepally Nagga Sai <141003177+WNS-avant@users.noreply.github.com>
2024-02-18 11:37:59 +0530

    Update CalcInterface.java

--- CalcInterface.java
+++ CalcInterface.java
@@ -8,10 +8,9 @@ public class CalcInterface
         System.out.println("               ==> Command Interface Calculator <==");
         System.out.println("      _________________________-----___________________________");
         System.out.println("");
-        System.out.println("Choose from Addition, Subtraction, Multiplication, Division, Power, or Comparison");
+        System.out.println("Choose from Addition, Subtraction, Multiplication, Division, Power(^), Factorial(!) or Comparison(<,>)");
         System.out.println("");
         System.out.println("Enter your expression in this format: for example, '2+3', or type 'exit' to quit.");
-
         while (true) 
         {
             System.out.print("-> ");
@@ -53,31 +52,59 @@ public class CalcInterface
     }
 
 	private static double evaluate(String input) 
-	{
-		String [] bit =	input.split("(?<=[+\\-*/^><])|(?=[+\\-*/^><])");
-		double operand1 =  Double.parseDouble(bit[0]);
-		String operator = bit[1];
-		double operand2 =  Double.parseDouble(bit[2]);
+	{    
+        if (input.contains("!")) 
+        {
+            String[] bit = input.split("!");
+            if (bit.length != 1) 
+            {
+                throw new IllegalArgumentException("Invalid factorial expression");
+            }
+            double operand = Double.parseDouble(bit[0]);
+            return factorial(operand);
+        } 
+        else 
+        {
+            String[] bit = input.split("(?<=[+\\-*/^><])|(?=[+\\-*/^><])");
+            if (bit.length != 3) 
+            {
+                throw new IllegalArgumentException("Invalid expression format");
+            }
+		    double operand1 =  Double.parseDouble(bit[0]);
+		    String operator = bit[1];
+		    double operand2 =  Double.parseDouble(bit[2]);
 	
-		switch(operator)
-		{
-			case "+" : return (operand1 + operand2);
-			case "-" : return (operand1 - operand2);
-			case "^" : return (Math.pow(operand1, operand2));
-			case "*" : return (operand1 * operand2);
-			case "/" : if(operand2!=0)
-						{
-							return (operand1 / operand2);
-						}
-						throw new ArithmeticException("Division by zero. Not a valid expression");
-			case "<": return compareOperands(operand1, operand2, operator) ? 1 : 0;
-			case ">": return compareOperands(operand1, operand2, operator) ? 1 : 0;
-	     
-			default: throw new NumberFormatException();
-	    }
+		    switch(operator)
+		    {
+			    case "+" : return (operand1 + operand2);
+			    case "-" : return (operand1 - operand2);
+			    case "^" : return (Math.pow(operand1, operand2));
+			    case "*" : return (operand1 * operand2);
+			    case "/" : if(operand2!=0)
+						    {
+							    return (operand1 / operand2);
+						    }
+						    throw new ArithmeticException("Division by zero. Not a valid expression");
+			    case "<": return compareOperands(operand1, operand2, operator) ? 1 : 0;
+			    case ">": return compareOperands(operand1, operand2, operator) ? 1 : 0;
+
+                default: throw new NumberFormatException();
+	        }
+        }
 	}
 
-	private static boolean compareOperands(double operand1, double operand2, String operator) 
+	private static double factorial(double operand) {
+        if (operand <= 1) {
+            return 1; 
+        }
+        double fact = 1;
+        for (double i = 1; i <= operand; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
+    private static boolean compareOperands(double operand1, double operand2, String operator) 
 	{
 	    switch (operator) 
 	    {
