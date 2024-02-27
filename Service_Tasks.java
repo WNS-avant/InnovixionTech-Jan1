7d72211d9ae66ce551c38e0352b236f1d11daf65
Waddepally Nagga Sai <141003177+WNS-avant@users.noreply.github.com>
2024-02-27 14:11:28 +0530

    Update Service_Tasks.java

--- Service_Tasks.java
+++ Service_Tasks.java
@@ -1,19 +1,1 @@
-package com.manager.service;
 
-import java.util.List;
-
-import com.manager.entity.Tasks;
-
-public interface Service_Tasks {
-
-public	String addTask(Tasks task);
-
-public	String removeTask(String title);
-
-public	List<Tasks> getTasks();
-
-public	String addToComplete(String title);
-
-public List<Tasks> getCompletedTasks();
-
-}
